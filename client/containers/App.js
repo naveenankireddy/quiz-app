42918fe2800d1637130c5c6826cbed9782154508
naveenankireddy <naveenkumarreddyankireddy@gmail.com>
2020-10-31 17:56:49 +0530

    setup node & react

---
+++ client/containers/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Route } from 'react-router-dom';
+import '../scss/index.scss';
+import { getCurrentUser, noToken } from '../actions'
+
+
+
+import HomePage from '../components/HomePage';
+import UserForm from '../components/AddImage';
+
+class App extends Component {
+  state = { 
+    token: ""
+  }
+
+  componentDidMount() {
+    var token = localStorage.getItem('authToken') || '';
+    if(token) {
+      this.setState({token: token})
+      this.props.dispatch(getCurrentUser())
+    } else {
+      this.props.dispatch(noToken());
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/users/add" component={UserForm} /> 
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    currentUser: state.currentUser.user
+  }
+}
+
+export default connect(mapStateToProps)(App);
+
+
+
+
+
+  
